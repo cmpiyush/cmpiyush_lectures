59226124db6f005b45052848e9aabdb831702efb
cmpiyush <cmpiyush2018@gmail.com>
2025-09-11 13:11:52 +0530

    Corrected and Updated notes name

--- subjects.js
+++ subjects.js
@@ -1,25 +1,20 @@
 export const structure = {
   "Foundations of Social Work": {
-    "Sociological Concepts": [
-      "Society-Basic Concept.md",
-      "Functionalism.md",
-      "Society as a system of relationship, Social Structure and Social Processes.md",
-      "Socialization- Meaning, process and significance.md",
-      "Social Institutions- Meaning, characteristics and types.md",
-      "Social Stratification- Meaning, characteristics and types.md",
-      "Society and Culture.md",
-      "Social Control- Meaning, concept and agencies, characteristics and types.md",
-      "Social Problems- Meaning, characteristics and types.md",
-      "Social Institutions.md",
-      "Social Change.md",
-      "Indian Society.md",
-      "Social change process in India.md"
-      ]
+    "Sociology": [
+      "Feminism.md",
+      "Modernity and Post-Modernity.md",
+      "Major Social Problems.md",
+      ],
+      "Political Economy": [
+        "Liberalism.md",
+        "Globalisation.md",
+        "State and Civil Society.md"
+      ],
   },
   "Introduction to Social Work": {
-    "Concept Objective and Scope":[
+    "Concept, Objectives, and Scope":[
         "The Concept of Social Work.md", 
-        "Principles, values and philosophy of  Social Work.md",
+        "Principles and values in Social Work.md",
         "Goals and Functions of Social Work.md",
         "System and Integrated Approach to  Social Work Practice.md",
         "Attributes of a profession.md",
@@ -65,11 +60,7 @@ export const structure = {
         "Population Growth and its Consequences.md",
         "Population Policy and Programmes.md",
         "Determinants of population growth.md",
-        "Population Control Measures.md",
-        "Role of social worker in family planning.md",
-        "Population education.md",
-        "population education in educational institutions.md",
-        "social work practice and population education.md"
+        "Role of social worker in family planning.md"
       ],
       "Population and India": [
         "Characteristics of Indian Population.md",
@@ -79,61 +70,29 @@ export const structure = {
         "National Population policy.md",
         "Family planning in India.md",
       ],
+      "Population Policy": [
+        "Population Control Measures.md",
+        "Population Policy.md",
+        "Family Planning.md"
+      ],
+      "Population Education": [
+        "Population education.md"
+      ],
       "Fertility": [
-        "Fertility- Meaning, Characteristics and Importance.md",
-        "Cause and Consequences of Fertility.md",
-        "Fertility in India.md"
+        "Fertility.md"
       ],
       "Mortality": [
         "Mortality- Meaning, Characteristics and Importance.md",
         "Causes and Consequences of Mortality.md",
         "Mortality in India.md"
       ],
-      "Migration": [
-        "Migration- Meaning, Characteristics and Importance.md",
-        "Causes and Consequences of Migration.md",
-        "Migration in India.md"
+      "Internal Migration": [
+        "Migration.md"
       ],
       "Immigration": [
         "Immigration- Meaning, Characteristics and Importance.md",
         "Causes and Consequences of Immigration.md",
         "Immigration in India.md"
       ]
-  },
-  "Statistics for Social Work": {
-      "Statistics": [
-        "Statistics-Meaning, Signification, Uses and limitations.md",
-        "Diagrammatic representation of data.md",
-        "Descriptive Statistics.md",
-        "Measures of Dispersion.md",
-        "Chi-Square Test.md",
-        "Co-efficient of correlation.md",
-        "Regression Analysis.md",
-      ]
-  },
-  "Social Work Theory and Approaches": {
-    "Cognitive-Behavioral Practice": [
-      "Cognitive-Behavioral Theory.md",
-      "Cognitive-Behavioral Techniques.md"
-    ],
-    "Systems Theory": [
-      "Systems Theory.md"
-    ],
-    "Ecology and Person-in-Environment Approach": [
-      "Ecological Systems Theory.md"
-    ],
-    "Psychodynamic and Humanistic Approaches": [
-      "Psychodynamic Theory.md",
-      "Humanistic Theory.md"
-    ],
-    "Conflict Theory": [
-      "Conflict Theory.md"
-    ],
-    "Feminist Theory and Anti-oppressive Theory": [
-      "Feminist Theory.md"
-    ],
-    "Strengths Perspective": [
-      "Strengths Perspective.md"
-    ]
   }
 }
